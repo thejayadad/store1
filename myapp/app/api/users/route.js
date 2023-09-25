c4e268df4fc42c0875a92cc83d2e4400a9e5e2a6
thejayadad <thejayadad@gmail.com>
2023-09-25 07:50:33 -0400

    update

---
+++ myapp/app/api/users/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import User from "@/models/User";
+import clientPromise from "@/lib/mongodb"
+
+
+
+export const GET = async (request)=> {
+    try {
+        const client = await clientPromise;
+        const db = client.db();
+
+        const users = await db.collection("users").find({}).toArray();
+
+        return new NextResponse(JSON.stringify(users), { status: 200 });
+      } catch (error) {
+        return new NextResponse("Database Error", { status: 500 });
+      }
+}
